4c66101a0f5580f0957531e87fc5826e9b70890f
Jips <Jips.ne@gmail.com>
2015-09-22 14:30:21 +0600

    [номер таска в YouyTrack] Start to implementing NavView

--- RemindMe/app/src/main/java/com/jdcorp/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/jdcorp/remindme/MainActivity.java
@@ -2,6 +2,7 @@ package com.jdcorp.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -9,13 +10,17 @@ import android.view.MenuItem;
  * Project RemindMe
  * Created by Евгений on 22.09.2015.
  */
-public class MainActivity extends Activity {
+
+/* Для тог, что бы тема работала, нужно заменить extends Activity на extends AppCompatActivity */
+public class MainActivity extends AppCompatActivity {
 
     /* Я хз зачем это нужно, но, объявляем переменную */
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /* Перед вызовом super.onCreate устанавливаем тему AppDefault */
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
